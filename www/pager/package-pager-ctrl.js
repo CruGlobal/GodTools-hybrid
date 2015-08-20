b36355419a3b186f41f26cfd154a6ca3c5d2e8c1
Spencer Oberstadt <soberstadt@gmail.com>
2015-08-20 16:01:08 -0400

    toggle language on swipe up

--- www/pager/package-pager-ctrl.js
+++ www/pager/package-pager-ctrl.js
@@ -1,10 +1,18 @@
 angular.module('GodTools')
-  .controller('PackagePagerCtrl', function() {
+  .controller('PackagePagerCtrl', function($translate) {
     this.showBars = false;
 
     this.toggleBars = function(value) {
       if(value === undefined)
         value = !this.showBars;
       this.showBars = value;
+    }
+
+    this.currentLang = 'en'
+    this.toggleLanguage = function() {
+      if(this.currentLang == 'en')
+        $translate.use(this.currentLang = 'zh')
+      else
+        $translate.use(this.currentLang = 'en')
     }
   })
