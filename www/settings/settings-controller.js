4d04164f353117a4b74de1dee6a272e745d901b0
Spencer Oberstadt <soberstadt@gmail.com>
2015-08-25 13:36:16 -0400

    save primary lang in localstorage

--- www/settings/settings-controller.js
+++ www/settings/settings-controller.js
@@ -1,8 +1,27 @@
 angular.module('GodTools')
-  .controller('SettingsCtrl', function(GTLanguages) {
-    this.languages = [{code: 'en', name: 'English', packages: ['kgp']}]
-    GTLanguages.languages().then(function(languages){
-      this.languages = languages;
-      console.log(this.languages)
-    })
-  })
+  .controller('SettingsCtrl', function($scope, GTLanguages) {
+    this.languages = [{code: 'en', name: 'English', packages: ['kgp']}];
+    this.primaryLang = "";
+    this.parallelLang = "None"
+    var that = this;
+
+    $scope.$on('$ionicView.beforeEnter', function(){
+      if(that.languages.length > 1) {
+        getLangNames()
+      }
+      else {
+        GTLanguages.languages().then(function(languages){
+          that.languages = languages;
+          getLangNames();
+        })
+      }
+    });
+
+    var getLangNames = function() {
+      var primaryCode = GTLanguages.getPrimary();
+      angular.forEach(that.languages, function(lang) {
+        if(lang.code == primaryCode)
+          that.primaryLang = lang.name;
+      })
+    };
+  });
