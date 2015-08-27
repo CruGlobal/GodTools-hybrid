8bf65e719e26cbd744d0f0afb92594a5d2db59ce
Spencer Oberstadt <soberstadt@gmail.com>
2015-08-27 17:19:46 -0400

    add popups for laws 3 & 4

--- www/kgp/kgp-panels.js
+++ www/kgp/kgp-panels.js
@@ -1,25 +1,25 @@
 window.kgpPanels = {
-  'kgp-s1-p1': {
+  'kgp-s2-p1': {
     template: '<div class="popup-verse">{{"8189a000-f7d3-4bc3-8eea-ef826c53dc5c" | translate}}</div>'+
     '<div class="popup-v-reference">{{"4919a9b7-f874-4362-87d5-c59ec8da754c" | translate}}</div>',
     color: 'cucumber'
   },
-  'kgp-s1-p2': {
+  'kgp-s2-p2': {
     template: '<div class="popup-verse">{{"90c1f8d1-39c0-49f8-b905-5bd7e850eef8" | translate}}</div>'+
     '<div class="popup-v-reference">{{"df32c4e8-e832-4026-870e-cbde62215c5b" | translate}}</div>',
     color: 'cucumber'
   },
-  'kgp-s2-p1': {
+  'kgp-s3-p1': {
     template: '<p>{{"7743b4d5-3f48-4c69-8062-3e2216cc66a6" | translate}}</p>'+
     '<p>{{"f3a5855f-56cc-4af4-8220-eef262aac39c" | translate}}</p>',
     color: 'punch'
   },
-  'kgp-s2-p2': {
+  'kgp-s3-p2': {
     template: '<div class="popup-verse">{{"03346729-5937-4bee-8f49-3511e473ffa0" | translate}}</div>'+
     '<div class="popup-v-reference">{{"b59b8b23-53cd-4097-86e4-819c619abde2" | translate}}</div>',
     color: 'punch'
   },
-  'kgp-s2-p3': {
+  'kgp-s3-p3': {
     template: '<p>{{"7f82556a-3890-487c-8c37-e498cb254285" | translate}}</p>'+
     '<p>{{"28acdb15-948c-4cc1-a619-4dfc088ace38" | translate}}</p>'+
     '<div class="popup-content-center"><img align="center" height="120" width="120" src="img/d3cea3849a1b57f0621ad1c6293b7935e409f67b.png" /></div>'+
@@ -27,5 +27,52 @@ window.kgpPanels = {
     '<p>{{"7f82556a-3890-487c-8c37-e498cb254285" | translate}}</p>'+
     '<p>{{"28acdb15-948c-4cc1-a619-4dfc088ace38" | translate}}</p>',
     color: 'punch'
-  }
+  },
+  'kgp-s4-p1': {
+    template: '<div class="popup-verse">{{"f70b16f3-6f64-41eb-855c-3144817079ef" | translate}}</div>'+
+    '<div class="popup-v-reference">{{"e14d6b3b-f1b0-4487-8dd8-ec26eb6c32c3" | translate}}</div>',
+    color: 'blue'
+  },
+  'kgp-s4-p2': {
+    template: '<div class="popup-verse">{{"e9cacbb8-f5ed-41b7-a088-352484fb3a6a" | translate}}</div>'+
+    '<div class="popup-v-reference">{{"0f0d51c7-d642-4b1d-98c0-1e6ce643180b" | translate}}</div>',
+    color: 'blue'
+  },
+  'kgp-s4-p3': {
+    template: '<p>{{"8accddf2-2b51-4e70-9442-d8370a95914f" | translate}}</p>'+
+    '<p>{{"d94a834a-bb44-4eaf-a32f-6825d403937f" | translate}}</p>',
+    color: 'blue'
+  },
+  'kgp-s4-p4': {
+    template: '<div class="popup-verse">{{"bd8f809c-9bff-4a44-8935-d511ce68cbfc" | translate}}</div>'+
+    '<div class="popup-v-reference">{{"6716296c-4d2d-47e1-b6b4-3670f06e47ff" | translate}}</div>',
+    color: 'blue'
+  },
+  'kgp-s4-p5': {
+    template: '<p>{{"ae5d8b0a-4cbe-4749-982c-e014ad4f652b" | translate}}</p>'+
+    '<div class="popup-content-center"><img align="center" height="120" width="120" src="img/f621f45b7ab7a8a7289ba1fe27976e24aa0f8888.png" /></div>'+
+    '<div class="popup-v-label-center popup-margin-bottom">{{"a8df4e44-e7a9-4f3a-adef-117a48676b09" | translate}}</div>'+
+    '<p>{{"c5e6b703-6978-4111-af16-3517e874da81" | translate}}</p>'+
+    '<p>{{"0c2d3c8d-351d-4167-ada4-3feb9ce0425c" | translate}}</p>',
+    color: 'blue'
+  },
+  'kgp-s5-p1': {
+    template: '<p class="popup-explain-title">{{"28a68eff-6a53-43bd-8464-bb217c41a8ad" | translate}}</p>'+
+    '<p class="popup-explain">{{"cf1d7e3e-02b7-4a0b-a1a2-96ecbad4e24d" | translate}}</p>'+
+    '<p class="popup-explain-title">{{"35525f3b-5fa8-405e-a67e-b67fc8eb95b3" | translate}}</p>'+
+    '<p class="popup-explain">{{"a4f50f2c-6d0d-4071-a3ef-521e86b28809" | translate}}</p>'+
+    '<p class="popup-explain-title">{{"7c973705-08f5-4521-8098-77abb07c2cf4" | translate}}</p>'+
+    '<p class="popup-explain">{{"1d10c00d-7f6c-47a4-9083-0f4bcfd4790c" | translate}}</p>',
+    color: 'eggplant'
+  },
+  'kgp-s5-p2': {
+    template: '<div class="popup-verse">{{"c252e95d-6815-4ee2-9d3b-34497691bdbe" | translate}}</div>'+
+    '<div class="popup-v-reference">{{"82564a55-938c-4d32-8209-dbcb5bdf04e9" | translate}}</div>',
+    color: 'eggplant'
+  },
+  'kgp-s5-p3': {
+    template: '<p>{{"1ff0a490-5b3d-45b4-bd7e-635c6d89b08a" | translate}}</p>'+
+    '<p>{{"ec410cc4-4eb3-441a-bd67-ad617cd805c6" | translate}}</p>',
+    color: 'eggplant'
+  },
 }
