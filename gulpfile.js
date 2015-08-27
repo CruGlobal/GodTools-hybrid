55733c44963a58ed5ffb8385cd830133ed795117
Spencer Oberstadt <soberstadt@gmail.com>
2015-08-27 10:50:11 -0400

    add gulp-prefixer and clean up scss some

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -17,6 +18,10 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass({
       errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
