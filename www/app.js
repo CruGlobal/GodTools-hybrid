8160ab5cb06f6708aa6038221805212be9442b96
Garrett Young <garrett.young17@gmail.com>
2016-01-07 11:42:11 -0500

    WP8.1 fixes added

--- www/app.js
+++ www/app.js
@@ -9,7 +9,7 @@ angular.module('GodTools', ['ionic', 'pascalprecht.translate', 'ngCookies', 'xml
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && !ionic.Platform.isWindowsPhone() && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
